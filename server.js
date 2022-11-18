7054434b0d41ba3475e164589964dfdbe58147cb
HCL <2814884095@qq.com>
2022-11-18 20:23:34 +0800

    基本的大文件传输和断点续传，没有对大小文件类型进行限制

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ app.use(uploader())
 
 app.post('/api/upload', async (req, res) => {
   const { name, size, type, offset, hash } = req.body
+  // express-fileupload
   const { file } = req.files
+
   // 扩展名
   const ext = extname(name)
   // 文件名
@@ -35,12 +37,14 @@ app.post('/api/upload', async (req, res) => {
       })
       return
     }
+    // file.data <Buffer 00 00 00 20 66 74 79 70  ... 65486 more bytes>
     await appendFile(filename, file.data)
     res.send({
       message: 'appended',
     })
     return
   }
+
   await writeFile(filename, file.data)
   res.send({
     message: 'created',
